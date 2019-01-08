a3f04de27f53ab0cc2e0ea2d665f238a52d467cb
Brian Kim <mkim26@babson.edu>
2019-01-07 22:48:40 -0500

    post_setup-post-finished

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,28 +1,27 @@
 class Api::V1::PostsController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
   def index
     render json: Post.all
-  end
-
-  def new
-    post = Post.new
   end
 
   def show
     render json: Post.find(params[:id])
   end
-
+ 
   def create
     post = Post.new(post_params)
 
     if post.save
-      flast[:notice] = 'Post was successfully created'
-      redirect_to post
+      flash[:notice] = 'Post was successfully created'
+      render json: post
+    else
+      render json: { error: post.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:body)
+    params.permit(:body)
   end
 end
--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Router, Route, Switch, browserHistory } from 'react-router'
 import HomeTile from '../components/HomeTile';
+import PostFormContainer from '../containers/PostFormContainer'
+
 
 
 export const App = (props) => {
   return (
     <Router history={browserHistory}>
       <Route path='/' component={HomeTile} />
+      <Route path='/posts/new' component={PostFormContainer} />
     </Router>
   );
 }
